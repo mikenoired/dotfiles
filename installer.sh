4850c66bd81ce559ef37c936b6ab198bdc677113
Mike Noired <unkstd@mail.ru>
2024-08-28 15:48:01 +0300

    Check for having git before installing

--- installer.sh
+++ installer.sh
@@ -8,6 +8,13 @@ echo
 export DOTFILES_PATH="$HOME/dotfiles"
 echo "👉 Cloning into: '$DOTFILES_PATH'"
 
-git --version
+echo "Check for having 'git' in your system..."
 
-git clone --depth 1 git@github.com:mikenoired/dotfiles.git "$DOTFILES_PATH"
+which git &> /dev/null
+if [ $? -eq 0 ]; then
+  echo "Git is installed. Installing dotfiles..."
+  git clone --depth 1 git@github.com:mikenoired/dotfiles.git "$DOTFILES_PATH"
+else
+  echo "Error: Git is not installed. Please install it before proceeding or run 'git --version' for git activation."
+  exit 1
+fi
